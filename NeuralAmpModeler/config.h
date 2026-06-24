1ee471c58a1c2c5017189965eb53cdf634649da5
Moses DaSilva <mosesdasilva99@gmail.com>
2026-06-23 21:27:45 -0400

    Add bundled Inter font resources

--- NeuralAmpModeler/config.h
+++ NeuralAmpModeler/config.h
@@ -58,6 +58,8 @@
 
 #define ROBOTO_FN "Roboto-Regular.ttf"
 #define MICHROMA_FN "Michroma-Regular.ttf"
+#define INTER_REGULAR_FN "Inter-Regular.ttf"
+#define INTER_SEMIBOLD_FN "Inter-SemiBold.ttf"
 
 #define GEAR_FN "Gear.svg"
 #define FILE_FN "File.svg"
